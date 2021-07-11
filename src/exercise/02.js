c2f6aa4368a2452c7f499428c8cb9c250026988d
Nelo Puchades Bresó <correo@nelo.dev>
2021-07-11 23:09:30 +0200

    Reac Hooks - EC 4 - Using ref and custom options in custom hook

--- src/exercise/02.js
+++ src/exercise/02.js
@@ -3,20 +3,37 @@
 
 import * as React from 'react'
 
-function useLocalStorageState(key, defaultValue = '') {
-  const [state, setState] = React.useState(
-    () => window.localStorage.getItem(key) || defaultValue,
-  )
+function useLocalStorageState(
+  key,
+  defaultValue = '',
+  {serialize = JSON.stringify, deserialize = JSON.parse} = {},
+) {
+  const [state, setState] = React.useState(() => {
+    const valueInLocalStorage = window.localStorage.getItem(key)
+    if (valueInLocalStorage) {
+      return deserialize(valueInLocalStorage)
+    }
+
+    return typeof defaultValue === 'function' ? defaultValue() : defaultValue
+  })
+
+  const prevKeyRef = React.useRef(key)
+
   React.useEffect(() => {
-    window.localStorage.setItem(key, state)
-  }, [key, state])
+    const prevKey = prevKeyRef.current
+    if (prevKey !== key) {
+      window.localStorage.removeItem(prevKey)
+    }
+
+    prevKeyRef.current = key
+    window.localStorage.setItem(key, serialize(state))
+  }, [key, serialize, state])
 
   return [state, setState]
 }
 
-function Greeting({initialName = '', initialAge = 25}) {
+function Greeting({initialName = ''}) {
   const [name, setName] = useLocalStorageState('name', initialName)
-  const [age, setAge] = useLocalStorageState('age', initialAge)
   // 🐨 initialize the state to the value from localStorage
   // 💰 window.localStorage.getItem('name') || initialName
   // const [name, setName] = React.useState(
@@ -35,25 +52,13 @@ function Greeting({initialName = '', initialAge = 25}) {
     setName(event.target.value)
   }
 
-  function handleAgeChange(event) {
-    setAge(event.target.value)
-  }
-
   return (
     <div>
       <form>
         <label htmlFor="name">Name: </label>
         <input value={name} onChange={handleChange} id="name" />
-        <label htmlFor="age">Age: </label>
-        <input value={age} onChange={handleAgeChange} id="age" />
       </form>
-      {name ? (
-        <strong>
-          Hello {name}, happy {age} years
-        </strong>
-      ) : (
-        'Please type your name'
-      )}
+      {name ? <strong>Hello {name}</strong> : 'Please type your name'}
     </div>
   )
 }
