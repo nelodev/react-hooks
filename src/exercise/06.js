28fee28bf2afb93b4d9207f42a285721d458691c
Nelo Puchades Bresó <correo@nelo.dev>
2021-07-24 16:08:17 +0200

    React Hooks - useEffect: HTTP Requests - Finished EC1

--- src/exercise/06.js
+++ src/exercise/06.js
@@ -7,15 +7,23 @@ import {PokemonForm} from '../pokemon'
 
 function PokemonInfo({pokemonName}) {
   const [pokemon, setPokemon] = React.useState(null)
+  const [error, setError] = React.useState(null)
 
   React.useEffect(() => {
     if (pokemonName === '') return
     setPokemon(null)
-    fetchPokemon(pokemonName).then(pokemonData => {
-      setPokemon(pokemonData)
-    })
+    fetchPokemon(pokemonName)
+      .then(pokemonData => setPokemon(pokemonData))
+      .catch(error => setError(error))
   }, [pokemonName])
 
+  if (error)
+    return (
+      <div role="alert">
+        There was an error:{' '}
+        <pre style={{whiteSpace: 'normal'}}>{error.message}</pre>
+      </div>
+    )
   if (!pokemonName) return 'Submit a pokemon'
   if (pokemonName && !pokemon) return <PokemonInfoFallback name={pokemonName} />
   return <PokemonDataView pokemon={pokemon} />
